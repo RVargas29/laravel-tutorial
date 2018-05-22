e02a0495699884e4f398475e0795a83e65ccd0e2
Randall Vargas <randall.vargas@iica.int>
2018-05-22 13:13:56 -0600

    Added the routing information to views and web routing.

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,10 +1,10 @@
 <nav class="navbar navbar-default">
     <div class="container-fluid">
         <div class="navbar-header">
-            <a href="#" class="navbar-brand">Getting Started</a>
+            <a href="{{ route('blog.index') }}" class="navbar-brand">Getting Started</a>
             <ul class="nav navbar-nav">
-                <li><a href="#">Blog</a></li>
-                <li><a href="#">About</a></li>
+                <li><a href="{{ route('blog.index') }}">Blog</a></li>
+                <li><a href="{{route('other.about')}}">About</a></li>
             </ul>
         </div>
     </div>
