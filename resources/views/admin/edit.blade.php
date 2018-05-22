74749c280de503532fc0d3834379e48370f0e189
Randall Vargas <randall.vargas@iica.int>
2018-05-22 14:04:35 -0600

    Added csrf token field to the create and edit views.

--- resources/views/admin/edit.blade.php
+++ resources/views/admin/edit.blade.php
@@ -6,12 +6,13 @@
             <form action="{{ route('admin.update') }}" method="post">
                 <div class="form-group">
                     <label for="title">Title</label>
-                    <input type="text" class="form-control" id="title" name="title">
+                    <input type="text" class="form-control" id="title" name="title" value="{{ $post['title'] }}">
                 </div>
                 <div class="form-group">
                     <label for="content">Content</label>
-                    <input type="text" class="form-control" id="content" name="content">
+                    <textarea name="content" id="content" rows="10" class="form-control">{{ $post['content'] }}</textarea>
                 </div>
+                {{ csrf_field() }}
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
         </div>
