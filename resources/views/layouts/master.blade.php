40ffe6a227043a4eacecd4cb944ae801540eb8fc
Randall Vargas <randall.vargas@iica.int>
2018-05-22 10:22:56 -0600

    Added the CSS references and the body container.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,10 +4,14 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" href="{{ URL::to('css/styles.css') }}">
         <title>Getting started</title>
     </head>
     <body>
-        @yield('content')
+        @include('partials/header')
+        <div class="container">
+            @yield('content')
+        </div>        
     </body>
 </html>
