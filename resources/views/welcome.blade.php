a74498585e1a522e3506526b12a7a77ab64b4ff3
Randall Vargas <randall.vargas@iica.int>
2018-05-22 10:23:19 -0600

    Created the welcome page.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,33 @@
 @extends('layouts.master')
 
 @section('content')
-    <h1>Some content</h1>
+    <div class="row">
+        <div class="col-md-12">
+            <p class="quote">Laravel tutorial</p>  
+        </div>
+    </div>    
+    <div class="row">
+        <div class="col-md-12">
+            <h1 class="post-title">Learning laravel</h1>
+            <p>This blog post will get you right on track with laravel</p>
+            <p><a href="#">Read more...</a></p>
+            <hr/>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <h1 class="post-title">The next steps</h1>
+            <p>Understanding the Basics is great, but you need to be able to make the next steps.</p>
+            <p><a href="#">Read more...</a></p>
+            <hr/>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <h1 class="post-title">Laravel 5.3</h1>
+            <p>Though announced as a "minor release", Laravel 5.3 ships with some very interesting additions and features.</p>
+            <p><a href="#">Read more...</a></p>
+            <hr/>
+        </div>
+    </div>
 @endsection
