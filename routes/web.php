e7f9d88dc5bf4d7bde4c9136a28997a983d4e5fe
Randall Vargas <randall.vargas@iica.int>
2018-05-22 15:00:55 -0600

    Added validation rules to the methods.

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,9 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
+|
+| To check available validation rules: https://laravel.com/docs/5.6/validation#available-validation-rules
+|
 */
 
 Route::get('/', function () {
@@ -43,7 +46,14 @@ Route::group(['prefix' => 'admin'], function() {
         return view('admin.create');
     })->name("admin.create");
     
-    Route::post('create', function(\Illuminate\Http\Request $request) {
+    Route::post('create', function(\Illuminate\Http\Request $request, \Illuminate\Validation\Factory $validator) {
+        $validation = $validator->make($request->all(), [
+            'title' => 'required|min:5',
+            'content' => 'required|min:10'
+        ]);
+        if ($validation->fails()) {
+            return redirect()->back()->withErrors($validation);
+        }
         return redirect()->route('admin.index')->with('info', 'Post created, title: ' . $request->input('title'));
     })->name('admin.create');
     
@@ -62,7 +72,14 @@ Route::group(['prefix' => 'admin'], function() {
         return view('admin.edit', ['post' => $post]);
     })->name("admin.edit");
     
-    Route::post('edit', function(\Illuminate\Http\Request $request) {
+    Route::post('edit', function(\Illuminate\Http\Request $request, \Illuminate\Validation\Factory $validator) {
+        $validation = $validator->make($request->all(), [
+            'title' => 'required|min:5',
+            'content' => 'required|min:10'
+        ]);
+        if ($validation->fails()) {
+            return redirect()->back()->withErrors($validation);
+        }
         return redirect()->route('admin.index')->with('info', 'Post edited, new title: ' . $request->input('title'));
     })->name('admin.update');
 });
