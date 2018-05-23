bc6f8d9d0d7fda4db442ec2e926594b2a4818671
Randall Vargas <randall.vargas@iica.int>
2018-05-23 14:50:55 -0600

    Added new Eloquend models and migrations.

--- app/Post.php
+++ app/Post.php
@@ -2,49 +2,9 @@
 
 namespace App;
 
-class Post
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
 {
-    public function getPosts($session) {
-        if(!$session->has('posts')) {
-            $this->createDummyData($session);
-        }
-        return $session->get('posts'); 
-    }
-
-    public function getPost($session, $id) {
-        if(!$session->has('posts')) {
-            $this->createDummyData($session);
-        }
-        return $session->get('posts')[$id];
-    }
-
-    public function addPost($session, $title, $content) {
-        if(!$session->has('posts')) {
-            $this->createDummyData($session);
-        }
-        $posts = $session->get('posts');
-        array_push($posts, ['title' => $title, 'content' => $content]);
-        $session->put('posts', $posts);
-    }
-
-    public function editPost($session, $id, $title, $content)
-    {
-        $posts = $session->get('posts');
-        $posts[$id] = ['title' => $title, 'content' => $content];
-        $session->put('posts', $posts);
-    }
-
-    private function createDummyData($session) {
-        $posts = [
-            [
-                'title' => 'Post #1',
-                'content' => 'This is the dummy text of post #1'
-            ],
-            [
-                'title' => 'Post #2',
-                'content' => 'This is the dummy text of post #2'
-            ]
-        ];
-        $session->put('posts', $posts);
-    }
+    protected $fillable = ['title', 'content'];
 }
