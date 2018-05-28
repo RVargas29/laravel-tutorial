4e00d60f4cb0bd65eb2ea2b1f29cd3d7d621bcc3
Randall Vargas <randall.vargas@iica.int>
2018-05-28 10:26:38 -0600

    Added the Like - Post relationship.

--- app/Like.php
+++ app/Like.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Like extends Model
 {
-    //
+    public function post() {
+        return $this->belongsTo('App\Post', 'post_id');//No es requerido 'post_id' ya que es el default de Laravel.
+    }
 }
--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = ['title', 'content'];
+
+    public function likes() {
+        return $this->hasMany('App\Like', 'post_id');//No es requerido 'post_id' ya que es el default de Laravel.
+    }
 }
