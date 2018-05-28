014f985e2f4986d719c2a7b195af5996412ba857
Randall Vargas <randall.vargas@iica.int>
2018-05-28 14:19:53 -0600

    Added the mutator and accessor for the posts.

--- app/Post.php
+++ app/Post.php
@@ -15,4 +15,12 @@ class Post extends Model
     public function tags() {
         return $this->belongsToMany('App\Tag', 'post_tag', 'post_id', 'tag_id')->withTimestamps();//Name of the pivot table is not required
     }
+    //Mutator: Modifies data before saving it to the database.
+    public function setTitleAttribute($value) {
+        $this->attributes['title'] = strtolower($value);
+    }
+    //Accessor: Modifies data after it is retrived from the database.
+    public function getTitleAttribute($value) {
+        return strtoupper($value);
+    }
 }
